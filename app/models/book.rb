1f0fe78e53880ed66374d216ca9286a756cb2d86
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-02-01 22:05:53 +0900

    add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+  scope :costly, -> { where( "price > ?", 3000) }
+  scope :written_about, ->(theme) { where( "name like ?", "%#{theme}%")}
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  # validate
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0}
 end
+
+
