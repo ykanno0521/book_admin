7b1c1a58ba74d99f149e18113646257e92787125
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-02-03 07:39:36 +0900

    create books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+end
