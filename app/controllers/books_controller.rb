b71230628b6fc515950946c0156995e879991935
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-02-04 07:59:23 +0900

    add destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,29 @@
 class BooksController < ApplicationController
 
+  # CSRF対策をOFFにする
+  protect_from_forgery except: [:destroy]
+  # @book.find(params[:id])を共通化してshowとdestroyアクション前に実行
+  before_action :set_book, only: [:show, :destroy]
+
   def show
     @book = Book.find(params[:id])
     respond_to do |format|
       format.html
       format.json
     end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to do |format|
+      format.html {redirect_to "/"}
+      format.json {head :no_content}
+    end
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 
 end
